b1e92c0837c343b32caf5d5e9f434f0323f3b828
Alberth Matos <alberth@matos.cc>
2026-06-19 23:07:26 -0400

    feat: Initial commit
    - Added identity file, using age-yubikey module
    - added secrets:
      - github rate limit key
      - github ssh login key

---
+++ secrets.nix
@@ -0,0 +1,14 @@
+let
+  # YubiKey identity — matches identityPaths in nix-config
+  alberth = "age1yubikey1qvqh2pk2pnzdf5mlrzeeseyf9axnz2l7g0mzum8ex5kg08euf8sh2mxv69q";
+  codex   = "age1rx38js86awlvzvm99x8qhnhd42cn9ytcudgqzm44u9qk9g79kqhs9jktky";
+
+  users = [ alberth ];
+  systems = [
+    codex
+  ];
+in
+{
+  "github-ssh-key.age".publicKeys = users ++ systems;
+  "github-ratelimit.age".publicKeys = users ++ systems;
+}
