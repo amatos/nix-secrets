66515a5362ec39e67eed4b0e0747cfcd8f3ea753
Alberth Matos <alberth@matos.cc>
2026-06-27 00:52:19 -0400

    chore: add cachix auth token secret

--- secrets.nix
+++ secrets.nix
@@ -3,6 +3,7 @@ let
   alberth = "age1yubikey1qtpg5lwewq75p68ru0n909uzkqddkhym2mkwp37h2fwkkgfdem05ssa4m6y";
   codex = "age1786r092jkepdahryx7t9kru8txuvreh3f2pgtvrv3u5hmjxjjy3st9udnl";
   gammu = "age12vhj5z6zepnz7uyzks23p6rgwa7rudja7ectsrl89zf96nnmfcnq264972";
+  porkchop = "";
 
   users = [ alberth ];
   systems = [
@@ -15,4 +16,5 @@ in
   "github-ratelimit.age".publicKeys = users ++ systems;
   "luadns.ini.age".publicKeys = users ++ systems;
   "tailscale-authkey.age".publicKeys = users ++ systems;
+  "cachix-authtoken.age".publicKeys = users ++ systems;
 }
