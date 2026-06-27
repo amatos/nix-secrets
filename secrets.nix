094c050cbf51e95a2f5829fdb3e79e28f0629966
Alberth Matos <alberth@matos.cc>
2026-06-27 16:09:38 -0400

    feat: rekeyed for porkchop

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,7 @@ let
   alberth = "age1yubikey1qtpg5lwewq75p68ru0n909uzkqddkhym2mkwp37h2fwkkgfdem05ssa4m6y";
   codex = "age1786r092jkepdahryx7t9kru8txuvreh3f2pgtvrv3u5hmjxjjy3st9udnl";
   gammu = "age12vhj5z6zepnz7uyzks23p6rgwa7rudja7ectsrl89zf96nnmfcnq264972";
-  porkchop = "age1yegmaunkewrxj3v6lt86nalta0xq5gq7dpcxrggqp8p7nlzdde4qsnq5jz";
+  porkchop = "age1kmpn0s8hqsgmsqn3zzc9k3yhe7j0tfwtqa5pesyrd84cky9uwp8szektc6";
 
   users = [ alberth ];
   systems = [
