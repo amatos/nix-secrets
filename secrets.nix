b585ecef62e6e649f8cd10f21d392c031e542db7
Alberth Matos <alberth@matos.cc>
2026-08-28 23:32:36 -0400

    feat: add smtp-relay-sasl-fastmail
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- secrets.nix
+++ secrets.nix
@@ -49,6 +49,7 @@ in
   # "example-password.age".publicKeys = [ alberth ] ++ allHosts;
   "services/certbot-luadns.age".publicKeys = allKeys;
   "services/github-ratelimit.age".publicKeys = allKeys;
+  "services/smtp-relay-sasl-fastmail.age".publicKeys = allKeys;
   "services/syncthing-gui.age".publicKeys = allKeys;
   "services/tailscale-authkey.age".publicKeys = allKeys;
   "services/unifi-api.age".publicKeys = allKeys;
