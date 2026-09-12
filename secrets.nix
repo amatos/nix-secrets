8d24a04388737ba18ef01d4cd1f05175f82cc2d2
Alberth Matos <alberth@matos.cc>
2026-09-12 08:51:27 -0400

    feat: add dyndns-luadns secret

--- secrets.nix
+++ secrets.nix
@@ -58,6 +58,7 @@ in
   "services/syncthing-gui-password.age".publicKeys = allKeys;
   "services/syncthing-gui-userid.age".publicKeys = allKeys;
   "services/tailscale-authkey.age".publicKeys = allKeys;
+  "services/dyndns-luadns.age".publicKeys = allKeys;
   "services/unifi-api.age".publicKeys = allKeys;
   "ssh/github-ssh-key.age".publicKeys = allKeys;
   "users/alberth-password.age".publicKeys = allKeys;
